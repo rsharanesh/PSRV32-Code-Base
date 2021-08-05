949fc661ac91f83580780829507c7fe1272d0512
J. Phani Jayanth <64090140+Jayanth2209@users.noreply.github.com>
2021-08-05 22:19:26 +0530

    Update execute.v

--- EX/execute.v
+++ EX/execute.v
@@ -1,22 +1,47 @@
 module execute(
-  input clk_i,
-  input reset_i,
+  input clk_i,  // CLOCK
+  input reset_i,  // RESET
   
-  input pcsrc_i,
-  input instruction_i,
+  input pcsrc_i,  // PC
+  input instruction_i,  // INSTRUCTION
   
-  input read_data1_i,
-  input read_data2_i,
-  input rs1_i,
-  input rs2_i,
-  input rd_i,
-  input aluop_i,
-  input offset_i,
+  input read_data1_i, // DATA READ FROM REGISTER SOURCE 1 - OP1
+  input read_data2_i, // DATA READ FROM REGISTER SOURCE 2 - OP2
+  input rs1_i,  // ADDRESS OF REGISTER SOURCE 1
+  input rs2_i,  // ADDRESS OF REGISTER SOURCE 2
+  input rd_i, // ADDRESS OF DESTINATION REGISTER
+  input aluop_i,  // ALU OPCODE
+  input offset_i, // IMMEDIATE/OFFSET
   
-  input alusrc,
-  input regdst,
+  input alusrc, // CONTROL SIGNAL TO SELECT OP2
+  input regdst, // CONTROL SIGNAL TO SELECT DESTINATION REGISTER
+  output isbeq, // CONTROL SIGNAL FOR BEQ AND SIMILAR INSTRUCTIONS
   
-  output isbeq,
-  output alu_result_o,
-  output read_data2_o,
-  output write_reg_o,
+  output alu_result_o,  // ALU RESULT 
+  output read_data2_o,  // DATA READ FROM REGISTER SOURCE 2 IS PASSED TO MEM
+  output write_reg_o, // REGISTER TO WRITE TO - PASSED TO MEM
+  output pc_ifbranch  // PC IF BRANCH IS TAKEN
+) 
+  
+  reg [31:0] op1, op2;
+  reg [31:0] alu_result_r;
+  
+  always(@posedge clk_i)
+    begin
+      if(reset_i)
+        begin
+          alu_result_o <= 'b0;
+          write_reg_o <= 'b0;
+          read_data2_o <= 'b0;
+          pc_ifbranch <= 'b0;
+        end
+      else begin 
+        
+          
+          
+          
+          
+  
+  
+  
+  
