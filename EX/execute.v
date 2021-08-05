042275bc302e760d21c2c38024304ff4936fe2ef
J. Phani Jayanth <64090140+Jayanth2209@users.noreply.github.com>
2021-08-05 21:48:11 +0530

    Update execute.v

--- EX/execute.v
+++ EX/execute.v
@@ -2,8 +2,21 @@ module execute(
   input clk_i,
   input reset_i,
   
+  input pcsrc_i,
+  input instruction_i,
+  
+  input read_data1_i,
+  input read_data2_i,
   input rs1_i,
   input rs2_i,
   input rd_i,
   input aluop_i,
-  input 
+  input offset_i,
+  
+  input alusrc,
+  input regdst,
+  
+  output isbeq,
+  output alu_result_o,
+  output read_data2_o,
+  output write_reg_o,
