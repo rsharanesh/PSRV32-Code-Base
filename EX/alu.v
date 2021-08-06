d672b3b725632239647580d5ef77f160f1c79074
J. Phani Jayanth <64090140+Jayanth2209@users.noreply.github.com>
2021-08-06 14:05:27 +0530

    Update alu.v

--- EX/alu.v
+++ EX/alu.v
@@ -1,7 +1,7 @@
 module alu(
   input [31:0] op1,
   input [31:0] op2,
-  input [3:0] aluop,
+  input [5:0] aluop,
   output [31:0] alu_result
 );
   
@@ -11,26 +11,17 @@ module alu(
     begin
       case(aluop)
         // ARITHMETIC AND LOGICAL OPERATIONS
-        4'b0000: result = op1 + op2;  // ADD
-        4'b0001: result = op1 << op2[3:0];
-        4'b0010: result = $signed(op1) < $signed(op2);
-        4'b0011: result = op1 < op2;
-        4'b0100: result = op1 ^ op2;  // XOR
-        4'b0101: result = op1 >> op2[3:0];
-        4'b0110: result = op1 | op2;  // OR
-        4'b0111: result = op1 & op2;  // AND
-        4'b1000: result = op1 - op2;  // SUB
-        4'b1101: result = $signed(op1) >>> op2[3:0];
-        
-        // BRANCH INSTRUCTIONS
-        4'b1001: result = op1 == op2; // BEQ
-        4'b1010: result = op1 != op2; // BNE
-        4'b1011: result = $signed(op1) < $signed(op2);  // BLT
-        4'b1100: result = $signed(op1) >= $signed(op2); // BGE
-        4'b1110: result = op1 < op2;  // BLTU
-        4'b1111: result = op1 >= op2; // BGEU
-        default: result = 32'bx // (OR SHOULD I PUT ZERO??)
-                  
+        6'd0: result = op1 + op2;  // ADD
+        6'd1: result = op1 << op2;  // SLL
+        6'd2: result = $signed(op1) < $signed(op2); // SLT
+        6'd3: result = op1 < op2; // SLTU
+        6'd4: result = op1 ^ op2;  // XOR
+        6'd5: result = op1 >> op2;  // SRL
+        6'd6: result = op1 | op2;  // OR
+        6'd7: result = op1 & op2;  // AND
+        6'd8: result = $signed(op1) >>> op2;  //SRA
+        6'd9: result = op1 - op2;  // SUB
+        default: result = 32'bx // (OR SHOULD I PUT ZERO??)            
       endcase
     end
     
