2c0c22befca64a8b73169639e39afba616e70e07
J. Phani Jayanth <64090140+Jayanth2209@users.noreply.github.com>
2021-08-06 13:21:22 +0530

    Update alu.v

--- EX/alu.v
+++ EX/alu.v
@@ -10,7 +10,26 @@ module alu(
   always @(*)
     begin
       case(aluop)
-                4'b0000: result = 
+        // ARITHMETIC AND LOGICAL OPERATIONS
+        4'b0000: result = op1 + op2;
+        4'b0001: result = op1 + op2;
+        4'b0010: result = op1 + op2;
+        4'b0011: result = op1 + op2;
+        4'b0100: result = op1 + op2;
+        4'b0101: result = op1 + op2;
+        4'b0110: result = op1 + op2;
+        4'b0111: result = op1 + op2;
+        4'b1000: result = op1 + op2;
+        4'b1101: result = op1 + op2;
+        
+        // BRANCH INSTRUCTIONS
+        4'b1001: result = op1 + op2;
+        4'b1010: result = op1 + op2;
+        4'b1011: result = op1 + op2;
+        4'b1100: result = op1 + op2;
+        4'b1110: result = op1 + op2;
+        4'b1111: result = op1 + op2;
+        default: result = 32'bx // OR SHOULD I PUT ZERO??
                   
       endcase
     end
