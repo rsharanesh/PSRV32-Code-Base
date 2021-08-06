8aa3016f72d6edb539a30aebaa37038ce6267c91
rsharanesh-iitm <ee19b116@smail.iitm.ac.in>
2021-08-06 23:04:52 +0530

    completed mem stage

--- Pipeline_Stages/pipeline_mem.v
+++ Pipeline_Stages/pipeline_mem.v
@@ -5,7 +5,24 @@ module memory(
     input [31:0] data_addr_i, //address of the memory cell to be read/written
     input [31:0] data_write_i, //data to be written to the memory cell
     input data_write_enable_i, //enable the data write
+    input data_read_enable_i, //enable the data read
 
     output [31:0] data_read_o, //data read from the memory cell
 );
 
+reg [31:0] data_memory_reg [0:255];
+reg [31:0] data_read_reg;
+
+always @(posedge clk) begin
+    if(~reset_i) begin
+        if(data_write_enable_i) begin
+            data_memory_reg[data_addr_i] <= data_write_i;
+        end
+        if(data_read_enable_i) begin
+            data_read_reg <= data_memory_reg[data_addr_i];
+        end
+    end
+end
+
+assign data_read_o = data_read_reg;
+endmodule
