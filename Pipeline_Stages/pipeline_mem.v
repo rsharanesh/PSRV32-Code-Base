d7b1ace8331c461f84e71403f6ac6b47bd073257
rsharanesh-iitm <ee19b116@smail.iitm.ac.in>
2021-08-06 22:58:24 +0530

    added mem stage

--- Pipeline_Stages/pipeline_mem.v
+++ Pipeline_Stages/pipeline_mem.v
@@ -2,5 +2,9 @@ module memory(
     input clk_i,//clock input
     input reset_i,//reset input
 
-    
+    input [31:0] data_addr_i, //address of the memory cell to be read/written
+    input [31:0] data_write_i, //data to be written to the memory cell
+    input data_write_enable_i, //enable the data write
+
+    output [31:0] data_read_o, //data read from the memory cell
 )
