6e361420fe7584bc8190cf884a335ca0e905ba18
J. Phani Jayanth <64090140+Jayanth2209@users.noreply.github.com>
2021-08-09 18:46:41 +0530

    Update alu.v

--- EX/alu.v
+++ EX/alu.v
@@ -21,7 +21,7 @@ module alu(
         6'd7: result_reg = op1_i & op2_i;  // AND
         6'd8: result_reg = $signed(op1_i) >>> op2_i;  //SRA
         6'd9: result_reg = op1_i - op2_i;  // SUB
-        default: result_reg = 32'bx // (OR SHOULD I PUT ZERO??)            
+        default: result_reg = 32'bx       
       endcase
     end
     
