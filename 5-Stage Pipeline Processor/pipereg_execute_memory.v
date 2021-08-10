cb80f240e26b545672e1ce6c2af482ac6869d9a1
Jayanth2209 <ee19b026@smail.iitm.ac.in>
2021-08-10 12:46:16 +0530

    Updated EM

--- 5-Stage Pipeline Processor/pipereg_execute_memory.v
+++ 5-Stage Pipeline Processor/pipereg_execute_memory.v
@@ -1,3 +1,18 @@
-module memory_writeback_register (
+module execute_memory_register (
+    input clk_i, //Clock input
+
+    input [31:0] pcsrc_i, //PC source input
+
+    input reg_write_i, //reg write (enables reg for writings)
+    input mem_read_i, //mem read
+
+    input [31:0] pc_new_i,
+    input pc_select_i, 
+
+    input [4:0] write_reg_i,
+    input [31:0] alu_result_i,  // ALU RESULT 
+    input [31:0] read_data2_i,
+
+    output 
 
 );
