ffe6b2c4f8550acfee8567f99a2360cd458a06ea
rsharanesh-iitm <ee19b116@smail.iitm.ac.in>
2021-08-14 19:04:39 +0530

    updated cpu.v

--- 5-Stage Pipeline Processor/cpu.v
+++ 5-Stage Pipeline Processor/cpu.v
@@ -16,3 +16,14 @@ wire [31:0] pc; // program counter
 wire [31:0] pc_src; // program counter source
 
 //from decode_pipeline
+wire [4:0] write_addr_reg; // write address register
+wire [31:0] write_data_reg; // write data register
+wire reg_write; // write register control signal
+wire [6:0] opcode; // instruction opcode
+wire [2:0] funct3; // instruction funct3
+wire [31:0] read_data1; // read data1
+wire [31:0] read_data2; // read data2
+wire [4:0] rs1; // rs1
+wire [4:0] rs2; // rs2
+wire [4:0] rd; // rd
+wire [31:0] offset; // offset
