10c64f7fd7f531ec3024a0e62d937bb7164b9978
rsharanesh2002 <ee19b116@smail.iitm.ac.in>
2022-01-01 14:59:20 +0530

    Fixed all the individual modules except fetch

--- 5-Stage Pipeline Processor/pipereg_memory_writeback.v
+++ 5-Stage Pipeline Processor/pipereg_memory_writeback.v
@@ -2,6 +2,7 @@ module memory_writeback_register (
     input clk_i,//clock input
     input reset_i,//reset input
 
+    input [31:0] pcsrc_i,//PC source
     input [31:0] mem_data_read_i,//data that was read from memory
     input [31:0] alu_result_i,//result of the ALU operation
     input [31:0] write_addr_reg_i, //address of the register to write to
@@ -9,6 +10,7 @@ module memory_writeback_register (
     input [1:0] dmem_to_reg_i,//memory to register muxsignal 
     input reg_write_i, //control signal to assert when writing to memory
 
+    output [31:0] mw_pcsrc_o; //PC source
     output [31:0] mw_mem_data_read_o, //data from memory
     output [31:0] mw_alu_result_o, //result of the ALU
     output [31:0] mw_write_addr_reg_o, //address of the register to write to
@@ -17,6 +19,7 @@ module memory_writeback_register (
     output mw_reg_write_o //control signal to assert when writing to memory
 );
 
+reg [31:0] mw_pcsrc_reg; 
 reg [31:0] mw_data_read_reg; 
 reg [31:0] mw_alu_result_reg;
 reg [31:0] mw_write_reg;
@@ -25,6 +28,7 @@ reg [1:0] mw_mem_to_reg;
 reg reg_write_reg;
 
 always @(posedge clk ) begin
+    mw_pcsrc_reg <= pcsrc_i;
     mw_data_read_reg <= mem_data_read_i;
     mw_alu_result_reg <= alu_result_i;
     mw_write_reg <= write_reg_i;
@@ -32,6 +36,7 @@ always @(posedge clk ) begin
     reg_write_reg <= reg_write_i;
 end
 
+assign mw_pcsrc_o = mw_pcsrc_reg;
 assign mw_mem_data_read_o = ~reset_i & mw_data_read_reg;
 assign mw_alu_result_o = ~reset_i & mw_alu_result_reg;
 assign mw_write_reg_o = ~reset_i & mw_write_reg;
