d6381417a7352dc6c6f53ed41d3d3ebf9877497e
rsharanesh2002 <ee19b116@smail.iitm.ac.in>
2022-01-02 00:30:49 +0530

    added basic testbench

---
+++ 5-Stage Pipeline Processor/testbench.v
@@ -0,0 +1,20 @@
+`timescale 1ns / 1ps
+
+`include "alu.v"
+`include "control.v"
+`include "cpu.v"
+`include "pipeline_fetch.v"
+`include "pipeline_decode.v"
+`include "pipeline_exceute.v"
+`include "pipeline_memory.v"
+`include "pipeline_writeback.v"
+`include "pipereg_fetch_decode.v"
+`include "pipereg_decode_exceute.v"
+`include "pipereg_exceute_memory.v"
+`include "pipereg_memory_writeback.v"
+
+module testbench;
+  reg clk;
+  reg reset;
+
+  
