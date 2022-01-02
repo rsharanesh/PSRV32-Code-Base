c0600cbf6965c5ab82daf222776f66de4b610ea3
Jayanth2209 <ee19b026@smail.iitm.ac.in>
2022-01-02 17:00:14 +0530

    EM Updated

--- 5-Stage Pipeline Processor/pipereg_execute_memory.v
+++ 5-Stage Pipeline Processor/pipereg_execute_memory.v
@@ -9,7 +9,7 @@ module execute_memory_register (
     input [31:0] pc_new_i,
     input pc_select_i,
 
-    input reg_write_i, //reg write (enables reg for writings)
+    //input reg_write_i, //reg write (enables reg for writings)
     input mem_read_i, //mem read
     input [1:0] dmem_to_reg_i, //mem2reg
     input mem_write_i, //mem write
@@ -23,7 +23,7 @@ module execute_memory_register (
     output [31:0] em_pc_new_o,
     output em_pc_select_o,
     
-    output em_reg_write_o,
+    //output em_reg_write_o,
     output em_mem_read_o,
     output [1:0] em_dmem_to_reg_o,
     output em_mem_write_o,
@@ -47,14 +47,13 @@ reg [31:0] execute_memory_read_data2_reg;
 
 always @(posedge clk_i) begin
     execute_memory_pcsrc_reg <= pcsrc_i;
+    execute_memory_pc_new_reg <= pc_new_i;
+    execute_memory_pc_select_reg <= pc_select_i;
 
-    execute_memory_reg_write_reg <= reg_write_i;
+    //execute_memory_reg_write_reg <= reg_write_i;
     execute_memory_mem_read_reg <= mem_read_i;
     execute_memory_dmem_to_reg_reg <= dmem_to_reg_i;
     execute_memory_mem_write_reg <= mem_write_i;
-
-    execute_memory_pc_new_reg <= pc_new_i;
-    execute_memory_pc_select_reg <= pc_select_i;
 
     execute_memory_write_addr_reg_reg <= write_addr_reg_i;
     execute_memory_alu_result_reg <= alu_result_i;
@@ -62,14 +61,13 @@ always @(posedge clk_i) begin
 end
 
 assign em_pcsrc_o = execute_memory_pcsrc_reg;
+assign em_pc_new_o = execute_memory_pc_new_reg;
+assign em_pc_select_o = execute_memory_pc_select_reg;
 
 assign em_reg_write_o = execute_memory_reg_write_reg;
 assign em_mem_read_o = execute_memory_mem_read_reg;
 assign em_dmem_to_reg_o = execute_memory_dmem_to_reg_reg;
 assign em_mem_write_o = execute_memory_mem_write_reg;
-
-assign em_pc_new_o = execute_memory_pc_new_reg;
-assign em_pc_select_o = execute_memory_pc_select_reg;
 
 assign em_write_addr_reg_o = execute_memory_write_addr_reg_reg;
 assign em_read_data2_o = execute_memory_read_data2_reg;
